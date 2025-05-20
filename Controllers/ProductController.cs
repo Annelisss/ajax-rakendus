be628b06b0cf973fc75ad548bd8b79edf1bc2eda
Annelisss <annelisepp@gmail.com>
2025-05-20 12:04:30 +0300

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AdvancedAjax.Models;
+
+namespace AdvancedAjax.Controllers
+{
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult SaveProduct([FromBody] Product product)
+        {
+            // Simuleerime andme salvestamist
+            return Json(new { success = true, message = $"Toode '{product.Name}' on salvestatud." });
+        }
+    }
+}
+
