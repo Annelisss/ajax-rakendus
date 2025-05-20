3877cb0f10dece601a3ab238350f3a8cb4a8f467
Annelisss <annelisepp@gmail.com>
2025-05-20 14:01:33 +0300

    Lisa City.cs mudel

---
+++ Models/City.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdvancedAjax.Models
+{
+    public class City
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(75)]
+        public string Name { get; set; }
+
+        public int CountryId { get; set; }
+
+        public string CountryName { get; set; } = "";
+    }
+}
